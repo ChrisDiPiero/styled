ae35a74f90ec13afc24c991ed5932b5da1e9c1c5
Chris DiPiero <frogmaster99@msn.com>
2022-09-04 10:44:10 -0500

    added styles to app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
+import styled from 'styled-components' //'styled' can be anything. is a 'default import', can be called anything. common practice is to use 'styled'
 
+//BasicTitle is a React component. Naming convention. see 'styled.h1' from above (default import).
+//'styled' is object. h1 is a method call
+
+const BasicTitle = styled.h1` 
+text-align: center;
+text-transform: capitalize;
+`
+//this is a 'tagged template literal'
 
 function App() {
   return (
-    <div style={{ padding: '2rem', backgroundColor: 'gray' }}>
-      <h1 className="title">styled components</h1>
+    <div style={{ padding: '2rem' }}>
+      <BasicTitle>styled components</BasicTitle>
       <button className="btn">click me</button>
     </div>
   );
